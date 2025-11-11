acfe513f47914f79ef1ddbb794b7c99d67a6d15d
Talha Rana <mtalharana093@gmail.com>
2025-11-11 16:38:40 +0500

    one to many relationship

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey
 from db import engine
 
 
@@ -17,20 +17,46 @@ class User(Base):
   phone : Mapped[str] = mapped_column(String, nullable=False)
   
   
-  def __repr__(self):
+  ## One to Many : User to Post
+  # Relationshiop user to post
+  posts: Mapped[list["Post"]] = relationship("Post", back_populates="user", cascade="all, delete")
+  
+  def __repr__(self)-> str:
     return f"<User(id={self.id}, name={self.name}, email={self.email})>"
+
+
+## Post Model
+class Post(Base):
+  __tablename__ = "Post"
   
-class Address(Base):
-  __tablename__ ="address"
+  id :Mapped[int] = mapped_column(primary_key=True)
+  user_id : Mapped[int] = mapped_column(ForeignKey("user_id", ondelete="CASECADE"), nullable=False)
+  title : Mapped[str] = mapped_column(String, nullable=False)
+  content : Mapped[str] = mapped_column(String, nullable=False)
   
-  id: Mapped[int] = mapped_column(primary_key=True)
-  street : Mapped[str] = mapped_column(String(50), nullable=False)
-  dist : Mapped[str] = mapped_column(String, nullable=False, unique=True)
-  county : Mapped[str] = mapped_column(String, nullable=False, unique=True)
+  # Relationship post to user
+  user : Mapped["User"] = relationship("User", back_populates="posts")
+  
+  
   
   
   def __repr__(self) -> str:
-    return f"Address(id={self.id!r}, street={self.street!r})"
+    return f"<Post(id={self.id}, title={self.name})>"
+  
+  
+  
+## Address Table
+# class Address(Base):
+#   __tablename__ ="address"
+  
+#   id: Mapped[int] = mapped_column(primary_key=True)
+#   street : Mapped[str] = mapped_column(String(50), nullable=False)
+#   dist : Mapped[str] = mapped_column(String, nullable=False, unique=True)
+#   county : Mapped[str] = mapped_column(String, nullable=False, unique=True)
+  
+  
+#   def __repr__(self) -> str:
+#     return f"Address(id={self.id!r}, street={self.street!r})"
   
   
 
