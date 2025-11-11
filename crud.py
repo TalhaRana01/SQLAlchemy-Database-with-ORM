56c899c704b2ca0145cf39f94af5c62a735e763b
Talha Rana <mtalharana093@gmail.com>
2025-11-11 22:47:44 +0500

    create first user

--- crud.py
+++ crud.py
@@ -1,1 +1,13 @@
 from users_models import User, Post
+from db import SessionLocal
+
+# Create User
+
+def create_user(name: str, email:str):
+  with SessionLocal() as session:
+    user = User(name=name, email=email)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+  
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # from models import create_tables , drop_table
 from users_models import create_table
+from crud import create_user
 
 
 create_table()
 # drop_table()
+
+
+# Users
+create_user("Talha Rana", "example@gmail.com")
